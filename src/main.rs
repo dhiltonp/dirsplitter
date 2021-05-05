0e8fe71d4a0e23b52559bde881e26752a1029745
David Hilton <david.hilton.p@gmail.com>
2021-05-05 17:01:20 -0400

    actual move, 32 photos per dir

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,9 @@ struct Cli {
     #[structopt(default_value = "splitter")]
     subdir_prefix: String,
 
-    #[structopt(default_value = "2", long)]
-    images_per_dir: usize
+    #[structopt(default_value = "32", long)]
+    images_per_dir: usize,
+    // todo: add recursive mode
 }
 
 /// Returns true if the path is a .jpg, .jpeg or .png, handling case sensitivity.
@@ -47,7 +48,7 @@ struct DirContents {
     images: Vec<path::PathBuf>,
 }
 
-fn read_dir(path: path::PathBuf) -> DirContents {
+fn read_dir(path: &path::PathBuf) -> DirContents {
     let mut subdirs = Vec::new();
     let mut images = Vec::new();
     if let Ok(entries) = fs::read_dir(&path) {
@@ -63,45 +64,63 @@ fn read_dir(path: path::PathBuf) -> DirContents {
             }
         }
     }
-    DirContents { path, subdirs, images }
+    DirContents {
+        path: path::PathBuf::from(path),
+        subdirs,
+        images,
+    }
 }
 
-fn split_dir(images: Vec<path::PathBuf>) {
+fn split_dir(dir_contents: DirContents, args: Cli) {
+    if dir_contents.images.len() > args.images_per_dir {
+        let mut new_dir_index = 0;
+        // I do not think initializing new_dir_path is necessary, but rust says it is.
+        //  Perhaps I have a flaw in my logic.
+        let mut new_dir_path = path::PathBuf::from(&dir_contents.path);
+        new_dir_path.push("uninitialized");
+        for i in 0..dir_contents.images.len() {
+            // create subdirs
+            if i % args.images_per_dir == 0 {
+                loop {
+                    new_dir_index += 1;
+                    let dir_name = format!("{}-{}", args.subdir_prefix, new_dir_index);
 
+                    new_dir_path = path::PathBuf::from(&dir_contents.path);
+                    new_dir_path.push(dir_name);
+                    if new_dir_path.exists() {
+                        continue;
+                    }
+                    match fs::create_dir(&new_dir_path) {
+                        Ok(_) => break,
+                        _ => panic!("Unable to create dir {:?}", new_dir_path),
+                    }
+                }
+            }
+            // move files to the new dir
+            let current_path = &dir_contents.images[i];
+            let file_name = match current_path.file_name() {
+                Some(file_name) => file_name,
+                _ => continue,
+            };
+            let mut new_path = path::PathBuf::from(&new_dir_path);
+            new_path.push(file_name);
+
+            match fs::rename(&current_path, &new_path) {
+                Ok(_) => (),
+                _ => panic!(
+                    "unable to move image from {:?} to {:?}",
+                    current_path, new_path
+                ),
+            }
+        }
+    }
 }
 
-/// Read a directory, recurse through it.
+/// Read a directory
 /// If there are < images_per_dir in that dir, leave it alone.
 /// Otherwise, split the dir.
 fn main() {
     let args = Cli::from_args();
-    let dir_contents = read_dir(args.path);
-
-    if dir_contents.images.len() > args.images_per_dir {
-        let mut new_dir_index = 0;
-        for i in 0..dir_contents.images.len() {
-            if i % args.images_per_dir == 0 {
-                loop {
-                    let mut new_dir_path = path::PathBuf::from(&dir_contents.path);
-                    let dir_name = format!("{}-{}", args.subdir_prefix, new_dir_index);
-                    new_dir_path.push(dir_name);
-                    new_dir_index += 1;
-                    match fs::create_dir(new_dir_path) {
-                        Ok(_) => break,
-                        _ => continue,
-                    }
-                }
-            }
-
-            // fs::rename()
-        }
-        // split images
-
-    }
-
-    // for dir in dir_contents.subdirs {
-    //     if dir.
-    // }
-    println!("{:?}", args.subdir_prefix);
-    println!("{:?}", args.images_per_dir);
+    let dir_contents = read_dir(&args.path);
+    split_dir(dir_contents, args);
 }
