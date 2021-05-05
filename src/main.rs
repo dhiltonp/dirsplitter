e7d266bfc4d83fd112df591d163e9778203186f2
David Hilton <david.hilton.p@gmail.com>
2021-05-05 16:17:36 -0400

    create subdirs

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,12 @@ use structopt::StructOpt;
 struct Cli {
     #[structopt(parse(from_os_str))]
     path: path::PathBuf,
-    // add subdir prefix
-    // add number of images per dir
-}
 
-#[derive(Debug)]
-struct DirContents {
-    subdirs: Vec<path::PathBuf>,
-    images: Vec<path::PathBuf>,
+    #[structopt(default_value = "splitter")]
+    subdir_prefix: String,
+
+    #[structopt(default_value = "2", long)]
+    images_per_dir: usize
 }
 
 /// Returns true if the path is a .jpg, .jpeg or .png, handling case sensitivity.
@@ -42,10 +40,17 @@ fn test_is_image() {
     assert!(is_image(path::PathBuf::from("blah.JPG")) == true);
 }
 
+#[derive(Debug)]
+struct DirContents {
+    path: path::PathBuf,
+    subdirs: Vec<path::PathBuf>,
+    images: Vec<path::PathBuf>,
+}
+
 fn read_dir(path: path::PathBuf) -> DirContents {
     let mut subdirs = Vec::new();
     let mut images = Vec::new();
-    if let Ok(entries) = fs::read_dir(path) {
+    if let Ok(entries) = fs::read_dir(&path) {
         for entry in entries.flatten() {
             let file_type = match entry.file_type() {
                 Ok(file_type) => file_type,
@@ -58,10 +63,45 @@ fn read_dir(path: path::PathBuf) -> DirContents {
             }
         }
     }
-    DirContents { subdirs, images }
+    DirContents { path, subdirs, images }
 }
 
+fn split_dir(images: Vec<path::PathBuf>) {
+
+}
+
+/// Read a directory, recurse through it.
+/// If there are < images_per_dir in that dir, leave it alone.
+/// Otherwise, split the dir.
 fn main() {
     let args = Cli::from_args();
-    println!("{:?}", read_dir(args.path));
+    let dir_contents = read_dir(args.path);
+
+    if dir_contents.images.len() > args.images_per_dir {
+        let mut new_dir_index = 0;
+        for i in 0..dir_contents.images.len() {
+            if i % args.images_per_dir == 0 {
+                loop {
+                    let mut new_dir_path = path::PathBuf::from(&dir_contents.path);
+                    let dir_name = format!("{}-{}", args.subdir_prefix, new_dir_index);
+                    new_dir_path.push(dir_name);
+                    new_dir_index += 1;
+                    match fs::create_dir(new_dir_path) {
+                        Ok(_) => break,
+                        _ => continue,
+                    }
+                }
+            }
+
+            // fs::rename()
+        }
+        // split images
+
+    }
+
+    // for dir in dir_contents.subdirs {
+    //     if dir.
+    // }
+    println!("{:?}", args.subdir_prefix);
+    println!("{:?}", args.images_per_dir);
 }
