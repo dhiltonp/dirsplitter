34fc77173b434dff6c5af64e4506b061c9dfb2cc
David Hilton <david.hilton.p@gmail.com>
2021-05-05 13:59:29 -0400

    get path from cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{fs, path};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(parse(from_os_str))]
+    path: path::PathBuf,
+}
+
+// struct Contents{
+//     dirs: Vec<path::PathBuf>,
+//     jpgs: Vec<path::PathBuf>,
+// }
+
+fn read_dirs(path: path::PathBuf) -> Vec<path::PathBuf> {
+    let mut subdirs = Vec::new();
+    subdirs.push(path);
+    subdirs
+}
+
 fn main() {
+    let args = Cli::from_args();
+    println!("{:?}", args.path);
+    println!("{:?}", read_dirs(args.path));
+    // fs::create_dir(args.path);
+    // for entry in fs::read_dir(args.path) {
+    //     // let path = entry.path();
+    //     println!("{:?}", entry);
+    // }
     println!("Hello, world!");
 }
