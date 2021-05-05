2e60d041653c1e0e80e7ac7ff6160c1453e8165e
David Hilton <david.hilton.p@gmail.com>
2021-05-05 14:28:07 -0400

    dir read with images

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::ffi::OsStr;
 use std::{fs, path};
 use structopt::StructOpt;
 
@@ -9,39 +10,59 @@ struct Cli {
     // add number of images per dir
 }
 
-// struct Contents{
-//     dirs: Vec<path::PathBuf>,
-//     jpgs: Vec<path::PathBuf>,
-// }
+#[derive(Debug)]
+struct DirContents {
+    subdirs: Vec<path::PathBuf>,
+    images: Vec<path::PathBuf>,
+}
 
-fn read_dirs(path: path::PathBuf) -> Vec<path::PathBuf> {
+/// Returns true if the path is a .jpg, .jpeg or .png, handling case sensitivity.
+fn is_image(file: path::PathBuf) -> bool {
+    let image_types = ["jpg", "jpeg", "png"];
+
+    let mut extension = match file.extension() {
+        Some(extension) => extension.to_ascii_lowercase(),
+        _ => return false,
+    };
+
+    for t in image_types.iter() {
+        if extension == OsStr::new(t) {
+            return true;
+        }
+    }
+
+    false
+}
+
+#[test]
+fn test_is_image() {
+    assert!(is_image(path::PathBuf::from("foo")) == false);
+    assert!(is_image(path::PathBuf::from("jpg")) == false);
+    assert!(is_image(path::PathBuf::from("blah.jpg")) == true);
+    assert!(is_image(path::PathBuf::from("blah.JPG")) == true);
+}
+
+fn read_dir(path: path::PathBuf) -> DirContents {
     let mut subdirs = Vec::new();
+    let mut images = Vec::new();
     if let Ok(entries) = fs::read_dir(path) {
         for entry in entries {
             if let Ok(entry) = entry {
-                println!("{:?}", entry.path());
                 if entry.file_type().unwrap().is_dir() {
                     subdirs.push(entry.path());
-                } else if entry.file_type().unwrap().is_file() {
-                    if entry.path().ends_with("jpg") {
-                        println!("image found");
-                        subdirs.push(entry.path());
-                    }
-                } else {
-                    println!("invalid data");
+                } else if entry.file_type().unwrap().is_file() && is_image(entry.path()) {
+                    images.push(entry.path());
                 }
             }
         }
     }
-
-    // for entry in fs::read_dir(args.path)
-    subdirs
+    DirContents {
+        subdirs: subdirs,
+        images: images,
+    }
 }
 
 fn main() {
     let args = Cli::from_args();
-    println!("{:?}", args.path);
-    println!("{:?}", read_dirs(args.path));
-    // fs::create_dir(args.path);
-    println!("Hello, world!");
+    println!("{:?}", read_dir(args.path));
 }
